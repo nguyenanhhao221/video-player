40607376c5708c310cddadbf8a45992836ddf16b
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-02-12 18:55:44 +0700

    add initial video display

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Video } from './components/Video.js';
+import { Menu } from './components/Menu.js';
+
+const VIDEOS = {
+  fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+  slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+  cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+  eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // in order to change state we need a function that call setState
+    this.chooseVideo = 
+
+    this.state = { src: VIDEOS.fast };
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Video Player</h1>
+        <Menu />
+        {/*pass in prop name src with value of App's state, src value */}
+        <Video src={this.state.src}/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />, 
+  document.getElementById('app')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
